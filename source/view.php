36ba3e50a022215552526cedc8119872be79aa42
Tobias Rahloff <rahloff@gmail.com>
2018-06-14 17:46:12 +0200

    init

---
+++ source/view.php
@@ -0,0 +1,120 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Prints a particular instance of newmodule
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_newmodule
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Replace newmodule with the name of your module and remove this line.
+
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/lib.php');
+
+$id = optional_param('id', 0, PARAM_INT); // Course_module ID, or
+$n  = optional_param('n', 0, PARAM_INT);  // ... newmodule instance ID - it should be named as the first character of the module.
+
+if ($id) {
+    $cm         = get_coursemodule_from_id('newmodule', $id, 0, false, MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $newmodule  = $DB->get_record('newmodule', array('id' => $cm->instance), '*', MUST_EXIST);
+} else if ($n) {
+    $newmodule  = $DB->get_record('newmodule', array('id' => $n), '*', MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $newmodule->course), '*', MUST_EXIST);
+    $cm         = get_coursemodule_from_instance('newmodule', $newmodule->id, $course->id, false, MUST_EXIST);
+} else {
+    error('You must specify a course_module ID or an instance ID');
+}
+
+require_login($course, true, $cm);
+
+$event = \mod_newmodule\event\course_module_viewed::create(array(
+    'objectid' => $PAGE->cm->instance,
+    'context' => $PAGE->context,
+));
+$event->add_record_snapshot('course', $PAGE->course);
+$event->add_record_snapshot($PAGE->cm->modname, $newmodule);
+$event->trigger();
+
+// Print the page header.
+
+$PAGE->set_url('/mod/newmodule/view.php', array('id' => $cm->id));
+$PAGE->set_title(format_string($newmodule->name));
+$PAGE->set_heading(format_string($course->fullname));
+
+/*
+ * Other things you may want to set - remove if not needed.
+ * $PAGE->set_cacheable(false);
+ * $PAGE->set_focuscontrol('some-html-id');
+ * $PAGE->add_body_class('newmodule-'.$somevar);
+ */
+
+// Output starts here.
+echo $OUTPUT->header();
+
+// Conditions to show the intro can change to look for own settings or whatever.
+if ($newmodule->intro) {
+    echo $OUTPUT->box(format_module_intro('newmodule', $newmodule, $cm->id), 'generalbox mod_introbox', 'newmoduleintro');
+}
+
+// Replace the following lines with you own code.
+echo $OUTPUT->heading('Yay! It works!');
+
+function yeha() {
+    echo '<script language="javascript">';
+    echo 'alert("invoked")';
+    echo '</script>';
+    
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://bt5eckr610.execute-api.eu-central-1.amazonaws.com/dev/message",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => "{\n\t\"name\": \"whatev\",\n\t\"email\": \"whatev\",\n\t\"message\": \"fuck u\"\n}",
+    CURLOPT_HTTPHEADER => array(
+        "Cache-Control: no-cache",
+        "Content-Type: application/json",
+        "Postman-Token: eefa4a64-4dfa-4c22-b865-66bb6eb20bbd"
+    ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+    echo "cURL Error #:" . $err;
+    } else {
+    echo $response;
+    }
+}
+
+yeha();
+
+// Finish the page.
+echo $OUTPUT->footer();
